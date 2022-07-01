92188567449f400ab950b5022f81c9aa5e7b5535
Harsh Kumar <64122322+harshror@users.noreply.github.com>
2022-07-01 17:57:52 +0530

    Add files via upload

---
+++ src/component/avengers.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class avengers extends Component {
+    render = () => {
+        return (
+            <body>
+                <div class="grid1">
+                    <div class="grid2"><img src="1.jpg" alt='1' />
+                        <h3>The avengers(2008)</h3>
+                    </div>
+                    <div class="grid2"><img src="2.jpg" alt='2' />
+                        <h3>The avengers(2010)</h3>
+                    </div>
+
+                    <div class="grid2"><img src="3.jpg" alt='3' />
+                        <h3>The avengers(2011)</h3>
+                    </div>
+                    <div class="grid2"><img src="4.jpg" alt='4' />
+                        <h3>The avengers(2015)</h3>
+                    </div>
+                    <div class="grid2"><img src="5.jpg" alt='5' />
+                        <h3>The avengers(2018)</h3>
+                    </div>
+                    <div class="grid2"><img src="6.jpg" alt='6' />
+                        <h3>The avengers(2020)</h3>
+                    </div>
+
+                </div>
+            </body>
+        );
+    }
+}
+
+export default avengers;
